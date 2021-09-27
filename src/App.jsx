aa42a0b039b2636d19d2634b0accd23c60506115
Yulissa Lopez <lyulissa5@gmail.com>
2021-09-26 22:08:58 -0500

    agregar componentes menu, iconos al index, modificar estilos imagen y descripcion

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,31 @@ import walmart from './media/walmart.png'
 
 function App() {
   return (
-
     <div className="App">
       <body>
           <header>
             <div className="logotipo">WILDLING</div>
             <nav>
                 <ul>
-                    <li>Manufacturing</li>
-                    <li>Packaging</li>
-                    <li>Figma</li>
-                    <li>Delivery</li>
+                    <MenuItem nombreitem="Manufacturing"/>
+                    <MenuItem nombreitem="Packaging"/>
+                    <MenuItem nombreitem="Figma"/>
+                    <MenuItem nombreitem="Delivery"/>
+                    
                 </ul>
             </nav>
             <div className="iconos">
-                <i className="far fa-user"></i>
-                <i className="fas fa-shopping-cart"></i>
-                <i className="fas fa-bars"></i>
+                <Iconos className ="far fa-user"/>
+                <Iconos className ="fas fa-shopping-cart"/>
+                <Iconos className ="fas fa-bars"/>
             </div>
         </header>
-
         <section>
             <h1>protecting the beauty</h1>
             <article>
                 <div className="descripcion">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum ipsam, cumque suscipit non saepe dolores perferendis fugit mollitia. eveniet nam quasi saepe praesentium magnam illo sequi facilis incidunt culpa recusandae cupiditate provident beatae non. Neque ea illo libero quo  </p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum ipsam, cumque suscipit non saepe dolores perferendis fugit mollitia. </p>
+                    <p>eveniet nam quasi saepe praesentium magnam illo sequi facilis incidunt culpa recusandae cupiditate provident beatae non. Neque ea illo libero quo  </p>
                     <div className="busqueda">
                         <input type="text" name="busqueda" placeholder="Looking for..."/>
                         <div className="lupa">
@@ -44,26 +44,38 @@ function App() {
                     <img src={munequita} alt="imagen de encabezado"/>
                 </div>
                 <div className="detalles">
-                    <i className="fab fa-facebook-f"></i>
-                    <i className="fab fa-twitter"></i>
-                    <i className="fab fa-instagram"></i>
+                    <Iconos className="fab fa-facebook-f"/>
+                    <Iconos className="fab fa-twitter"/>
+                    <Iconos className="fab fa-instagram"/>
                 </div>
             </article>
         </section>
-
-
         <footer>
             <h3>some of our lenders</h3>
             <div className="marcas">
-                <img src={asana} alt="asana"/>
-                <img src={chase} alt="chase"/>
-                <img src={buzzfeed} alt="buzzfeed"/>
-                <img src={walmart} alt="walmart"/>
+                <Marcas imagen = {asana} nombre="asana"/>
+                <Marcas imagen = {chase} nombre="chase"/>
+                <Marcas imagen = {buzzfeed} nombre="buzzfeed"/>
+                <Marcas imagen = {walmart} nombre="walmart"/>
             </div>
         </footer>
       </body>  
     </div>
   );
 }
+
+function MenuItem(props){
+    return (<li>{props.nombreitem}</li>);
+}
+
+function Iconos(props){
+    return(<i className={props.className}></i>);
+}
+
+function Marcas(props){
+    return(<img src={props.imagen} alt={props.nombre}/>);
+}
+
+
 
 export default App;
