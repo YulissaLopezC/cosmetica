945641c684b759b5466bbae97a904cdaf75ca2fc
Yulissa Lopez <lyulissa5@gmail.com>
2021-09-26 19:41:14 -0500

    se modifico app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <body>
           <header>
-            <div class="logotipo">WILDLING</div>
+            <div className="logotipo">WILDLING</div>
             <nav>
                 <ul>
                     <li>Manufacturing</li>
@@ -20,33 +20,33 @@ function App() {
                     <li>Delivery</li>
                 </ul>
             </nav>
-            <div class="iconos">
-                <i class="far fa-user"></i>
-                <i class="fas fa-shopping-cart"></i>
-                <i class="fas fa-bars"></i>
+            <div className="iconos">
+                <i className="far fa-user"></i>
+                <i className="fas fa-shopping-cart"></i>
+                <i className="fas fa-bars"></i>
             </div>
         </header>
 
         <section>
             <h1>protecting the beauty</h1>
             <article>
-                <div class="descripcion">
+                <div className="descripcion">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cum ipsam, cumque suscipit non saepe dolores perferendis fugit mollitia. eveniet nam quasi saepe praesentium magnam illo sequi facilis incidunt culpa recusandae cupiditate provident beatae non. Neque ea illo libero quo  </p>
-                    <div class="busqueda">
+                    <div className="busqueda">
                         <input type="text" name="busqueda" placeholder="Looking for..."/>
-                        <div class="lupa">
-                            <i class="fas fa-search"></i>
+                        <div className="lupa">
+                            <i className="fas fa-search"></i>
                         </div>
                     </div>
                     <button>view detail</button>
                 </div>
-                <div class="imagen">
+                <div className="imagen">
                     <img src={munequita} alt="imagen de encabezado"/>
                 </div>
-                <div class="detalles">
-                    <i class="fab fa-facebook-f"></i>
-                    <i class="fab fa-twitter"></i>
-                    <i class="fab fa-instagram"></i>
+                <div className="detalles">
+                    <i className="fab fa-facebook-f"></i>
+                    <i className="fab fa-twitter"></i>
+                    <i className="fab fa-instagram"></i>
                 </div>
             </article>
         </section>
@@ -54,7 +54,7 @@ function App() {
 
         <footer>
             <h3>some of our lenders</h3>
-            <div class="marcas">
+            <div className="marcas">
                 <img src={asana} alt="asana"/>
                 <img src={chase} alt="chase"/>
                 <img src={buzzfeed} alt="buzzfeed"/>
